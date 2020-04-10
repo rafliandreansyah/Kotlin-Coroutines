497a7223eec752c744e37077fa7a6bd516635464
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-04-10 09:37:17 +0700

    Initial commit

---
+++ app/src/main/java/com/azhara/kotlin_coroutines/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.azhara.kotlin_coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
