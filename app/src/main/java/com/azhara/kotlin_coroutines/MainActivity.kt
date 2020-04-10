c2eb62f6f75d6ad5c967c08dcc2a5ddf99d0df4e
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-04-10 12:49:36 +0700

    Membuat corountine dan menentukannya dimana dia berjalan. disini menggunakan IO karena melakukan simulsi request api dari internet dengan memasukkan waktu 1 detik

--- app/src/main/java/com/azhara/kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/azhara/kotlin_coroutines/MainActivity.kt
@@ -2,7 +2,11 @@ package com.azhara.kotlin_coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +15,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            /* Coroutine memiliki 3 Scope IO,Main, dan Default
+                IO: Berjalan pada Input Output seperti melakukan request api dari ke server atau
+                    melakukan request pada database local seperti SQLite
+                Main: Berjalan pada MainThread (thread utama) seperti berinteraksi dengan UI
+                Default: Digunakan untuk proses yang berat maka Corountine akan menentukan dimana corountine itu berjalan
+                    secara otomatis
+             */
+            CoroutineScope(Dispatchers.IO).launch {
+
+            }
+        }
+    }
+
+    private suspend fun fakeApiRequest(){
+        val result1 = getRequesApi1()
+        println("debug: $result1")
     }
 
     private suspend fun getRequesApi1(): String{
