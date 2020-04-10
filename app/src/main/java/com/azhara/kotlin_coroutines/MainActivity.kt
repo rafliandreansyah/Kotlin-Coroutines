08512ba89c0faede6551a9457046abf05dbb451e
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-04-10 12:38:45 +0700

    Fungsi suspend menandakan bahwa fungsi itu berjalan secara asynchronous dan dipanggil pada corountine

--- app/src/main/java/com/azhara/kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/azhara/kotlin_coroutines/MainActivity.kt
@@ -2,12 +2,21 @@ package com.azhara.kotlin_coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT1 = "RESULT#1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private suspend fun getRequesApi1(): String{
+        logThread("getRequesApi1")
+        delay(1000)
+        return RESULT1
     }
 
     private fun logThread(methodName: String){
